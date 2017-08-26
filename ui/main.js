5fdeb60cc6a23137c4f3ec1e3930b5ac0680b5c8
sita6 <30767383+sita6@users.noreply.github.com>
2017-08-26 19:03:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //write the comments
 var element=document.getElementById("main-text");
 element.innerHTML="smita is great";
+//write a code to change the image
+var img=document.getElementById("mandi");
+img.onclick=function(){
+    img.style.marginLeft='100px';
+    
+    };
