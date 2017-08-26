c6e5ebd4b932ecedd46b091252dae0bf3cef5b80
sita6 <30767383+sita6@users.noreply.github.com>
2017-08-26 12:06:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('This is my first article');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 
 });
 app.get('/article-second',function(req,res){
