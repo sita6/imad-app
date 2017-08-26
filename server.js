9c8617d6babec983272ca94b42651a985e46bb4e
sita6 <30767383+sita6@users.noreply.github.com>
2017-08-26 14:14:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
     res.send(createTemplate(article[articleName]));
 });
 app.get('/article-second',function(req,res){
