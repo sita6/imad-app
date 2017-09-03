4e1c7aab079045ea09b5f6ef4170e37dbaf614c9
sita6 <30767383+sita6@users.noreply.github.com>
2017-09-03 18:36:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/:articleName',function(req,res){
 var counter=0;
 
 app.get('/counter',function(req,res){
-    counter=counter+1;
+    counter=counter + 1;
     res.send(counter.toString());
 });
 app.get('/article-third',function(req,res){
