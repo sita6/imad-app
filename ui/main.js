b9018cea4a553e6b655f0b095adef72c73fd467c
sita6 <30767383+sita6@users.noreply.github.com>
2017-09-03 18:16:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++
@@ -1,9 +0,0 @@
-var button=document.getElementById("counter");
-var count=0;
-button.onclick=function()
-{
-    counter=counte+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
-    
-};
