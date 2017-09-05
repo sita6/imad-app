bf2b5f93e8dc084a44d1f94f58c458191192c63e
sita6 <30767383+sita6@users.noreply.github.com>
2017-09-05 12:18:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,23 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-    counter=counter+1;
-    var span =document.getElementById('count');
-    span.innerHTML=counter.toString();
+   //make a request
+var request =new XMLHtmlRequest();
+//capture the response and store it in the variable
+request.onreadystatechange=function(){
+    if (request.readyState==XMLHtmlRequest.Done){
+        if(request.status==200){
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+            
+            
+        }
+    }
+    
 };
+//make a request
+request.open('GET','http://http://smitan.imad.hasura-app.io/',true);
+request.send(null);
+};
+
